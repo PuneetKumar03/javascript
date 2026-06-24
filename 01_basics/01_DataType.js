6864dfce3ec617fbb23d38e6b48b2f14c874c833
PuneetKumar03 <p2401837@gmail.com>
2026-06-24 10:40:39 +0000

    Variable and DataType Conversion

---
+++ 01_basics/01_DataType.js
@@ -0,0 +1,24 @@
+let user = "Puneet"
+let isLoggedIn = true;
+let logIn = null;
+let logger;
+
+//console.log(typeof(isLoggedIn))
+
+//conversion of datatype
+
+let num = 33
+let validString = String(num)
+//console.log(typeof(validString))
+
+
+let string = "3abc"
+// let validNumber = Boolean(string)
+// console.log(typeof(valideNumber))
+// console.log(validNumber)
+
+let Null = null
+console.log(typeof(Null))
+let validNumber = Boolean(Null)
+console.log(validNumber)
+console.log(typeof(validNumber))
---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,7 @@
+const accountId = 14443
+let accountEmail = "puneet@gamil.com"
+var accountHolder = "Puneet"
+account = 1234
+console.log(account)
+console.log(accountHolder)
+console.table([accountId,accountEmail])
