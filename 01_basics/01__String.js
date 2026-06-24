0b4ee77ca19d0ad40f1413ba0a0275fa478ac565
PuneetKumar03 <p2401837@gmail.com>
2026-06-24 13:42:23 +0000

    String Operations

---
+++ 01_basics/01__String.js
@@ -0,0 +1,20 @@
+let str = new String("Puneet")
+
+console.log(str.substr(3))
+
+console.log(str.substring(0,5))
+
+console.log(str.slice(-5,-2))
+
+let url = "puneet1234@gm@ail.com"
+
+console.log(url.split('@'))
+
+console.log(url.replaceAll('@',''))
+
+let url1 = "  Puneet  "
+
+console.log(url1.trimRight().length)
+
+console.log(url1.length)
+
